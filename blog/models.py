b009b0612b87eac39fdc72e2ea1880a262a70574
Georgia Vlassi <georgia.vlassi@nokia.com>
2018-02-27 11:58:01 +0200

    QuerySet

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
 		('published', 'Published'),
-)
+	)
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250,unique_for_date='publish')
 	author = models.ForeignKey(User,related_name='blog_posts')
@@ -15,6 +20,13 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+	objects = models.Manager()
+	published = PublishedManager()
+	
+	def get_absolute_url(self):
+		return reverse('blog:post_detail',
+			args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug])
+
 
 class Meta:
 	ordering = ('-publish',)
